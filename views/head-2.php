0c47d6895c152d12af6e8ab041cf312491c124fe
Brad Sawicki <git@bradsawicki.com>
2013-09-16 10:49:21 -0500

    Googz Analytics / JS removed

--- views/head-2.php
+++ views/head-2.php
@@ -1,5 +1,4 @@
 	<link href="css/fonts/icons.css" rel="stylesheet" media="all">
 	<link href="css/styles.css" rel="stylesheet" media="all">
 	<link rel="stylesheet" type="text/css" href="//cloud.typography.com/7766032/711062/css/fonts.css" />
-	<script src="js/modernizr.js"></script>
 </head>
